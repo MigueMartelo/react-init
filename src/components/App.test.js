745890d1b4400df7b8e43c194d4e9d2ee4e49d93
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-06-30 11:35:11 -0500

    Config Jest OK

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { exportAllDeclaration } from '@babel/types';
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
 
-test('Hello world', () => {
-  const { getByText } = render(<p>Hello Jest</p>);
-  expect(getByText('Hello Jest')).toBeTruthy();
+describe('Test Jest config', () => {
+  test('Hello world', () => {
+    const { getByText } = render(<p>Hello Jest</p>);
+    expect(getByText('Hello Jest')).toBeTruthy();
+  });
+
+  test('App component', () => {
+    const title = 'Hello React';
+    const { getByText } = render(<App title={title} />);
+
+    expect(getByText('Hello React')).toBeTruthy();
+  });
+
+  test('Alt in img', () => {
+    expect(screen.findAllByAltText('Logo React')).toBeTruthy();
+  });
 });
